6db4f2d3f76b4da261b06f7c078a5002529785ad
DanubioLima <danubiovieiralima@gmail.com>
2020-03-24 18:36:06 -0300

    code

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = express.Router();
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileOngController = require('./controllers/ProfileOngController');
+const SessionController = require('./controllers/SessionController');
+
+
+routes.post('/session', SessionController.create)
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+routes.get('/profile',ProfileOngController.index);
+
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete );
+
+module.exports = routes;
